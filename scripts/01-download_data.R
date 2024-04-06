163f2282d96387dc509185250c70af78f1e5d645
anggimude <hyuk.jang@mail.utoronto.ca>
2024-04-05 20:36:47 -0400

    code finalized

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,25 +1,46 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from 
 # Author: Hyuk Jang
-# Date: 26 Mar 2024
+# Date: 18 April 2024
 # Contact: hyuk.jang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# Any other information needed? 
+# Pre-requisites: install packages 'readr' and 'tidyverse'.
+ 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(readr)
 library(tidyverse)
-
 
 #### Download data ####
 
+# URL to download data
+url_1 <- "https://apps.who.int/gho/athena/data/GHO/SDGSUICIDE?filter=COUNTRY:*;REGION:*;;AGEGROUP:YEARS15-24;AGEGROUP:YEARS25-34;AGEGROUP:YEARS35-44;AGEGROUP:YEARS45-54;AGEGROUP:YEARS55-64;AGEGROUP:YEARS65-74;AGEGROUP:YEARS75-84;AGEGROUP:YEARS85PLUS&ead=&x-sideaxis=COUNTRY;SEX&x-topaxis=GHO;YEAR;AGEGROUP&profile=crosstable&format=csv"
 
+# Set the filename to save the downloaded data
+suicide_age <- "suicide_ag_data.csv"
+
+# Download the file
+download.file(url_1, suicide_age, method = "auto")
+
+# Read the downloaded CSV file
+suicide_ag_data <- read_csv(suicide_age)
+
+# URL to download data
+url_2 <- "https://apps.who.int/gho/athena/data/xmart.csv?target=GHO/MH_12&profile=crosstable&filter=COUNTRY:*;REGION:*&ead=&x-sideaxis=COUNTRY;SEX&x-topaxis=GHO;YEAR"
+
+# Set the filename to save the downloaded data
+suicide_year <- "suicide_raw_data.csv"
+
+# Download the file
+download.file(url_2, suicide_year, method = "auto")
+
+# Read the downloaded CSV file
+suicide_raw_data <- read_csv(suicide_year)
 
 
 #### Save data ####
 
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# Write the downloaded data to a CSV file
+write_csv(suicide_raw_data, "data/raw_data/suicide_raw_data.csv") 
 
-         
+write_csv(suicide_ag_data, "data/raw_data/suicide_ag_data.csv") 

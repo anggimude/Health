69f280fcb7673d96f94a913466b34e7a5ddff7b6
anggimude <hyuk.jang@mail.utoronto.ca>
2024-04-07 04:37:03 -0400

    final changes

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -17,24 +17,24 @@ library(tidyverse)
 url_1 <- "https://apps.who.int/gho/athena/data/GHO/SDGSUICIDE?filter=COUNTRY:*;REGION:*;;AGEGROUP:YEARS15-24;AGEGROUP:YEARS25-34;AGEGROUP:YEARS35-44;AGEGROUP:YEARS45-54;AGEGROUP:YEARS55-64;AGEGROUP:YEARS65-74;AGEGROUP:YEARS75-84;AGEGROUP:YEARS85PLUS&ead=&x-sideaxis=COUNTRY;SEX&x-topaxis=GHO;YEAR;AGEGROUP&profile=crosstable&format=csv"
 
 # Set the filename to save the downloaded data
-suicide_age <- "suicide_ag_data.csv"
+suicide_age <- "data/raw_data/suicide_ag_data.csv"
 
 # Download the file
 download.file(url_1, suicide_age, method = "auto")
-
-# Read the downloaded CSV file
-suicide_ag_data <- read_csv(suicide_age)
 
 # URL to download data
 url_2 <- "https://apps.who.int/gho/athena/data/xmart.csv?target=GHO/MH_12&profile=crosstable&filter=COUNTRY:*;REGION:*&ead=&x-sideaxis=COUNTRY;SEX&x-topaxis=GHO;YEAR"
 
 # Set the filename to save the downloaded data
-suicide_year <- "suicide_raw_data.csv"
+suicide_year <- "data/raw_data/suicide_raw_data.csv"
 
 # Download the file
 download.file(url_2, suicide_year, method = "auto")
 
-# Read the downloaded CSV file
+#### Read data ####
+
+# Read the downloaded CSV files
+suicide_ag_data <- read_csv(suicide_age)
 suicide_raw_data <- read_csv(suicide_year)
 
 
@@ -44,4 +44,3 @@ suicide_raw_data <- read_csv(suicide_year)
 write_csv(suicide_raw_data, "data/raw_data/suicide_raw_data.csv") 
 
 write_csv(suicide_ag_data, "data/raw_data/suicide_ag_data.csv") 
-
